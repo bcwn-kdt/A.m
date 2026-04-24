eff55df3f2fa3550010d636b6b85d21463ea775c
bcwn-kdt <blackcatwhitenight@gmail.com>
2026-04-24 16:25:35 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/meodenkhongngu',
+  images: {
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig;
